2f8540162e0fff67c8394afc22d852ddfdf3c297
elizeurs <13ders@gmail.com>
2019-08-31 22:17:30 -0300

    Initial Commit

---
+++ KanoaSushi/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  KanoaSushi
+//
+//  Created by Elizeu RS on 31/08/19.
+//  Copyright © 2019 elizeurs. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+  override func viewDidLoad() {
+    super.viewDidLoad()
+    // Do any additional setup after loading the view.
+  }
+
+
+}
+
